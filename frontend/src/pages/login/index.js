d9690f4d4ffb6934479040ea069b10b90134faf7
Vinicius de Lima Mendonca <vinicius.mendoncaa@gmail.com>
2022-12-08 21:13:41 -0300

    Alterado for para htmlFor

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -8,9 +8,9 @@ export default function Login() {
             <h1>Login</h1>
 
             <div className="container">
-                <label for="email">Email</label>
+                <label htmlFor="email">Email</label>
                 <input className="form-control mb-2" type="email" id="email" placeholder="Insira o seu e-mail" />
-                <label for="senha">Senha</label>
+                <label htmlFor="senha">Senha</label>
                 <input className="form-control" type="password" id="senha" placeholder="******" />
                 <div className="botoes">
                     <button className="btn btn-primary mt-3">Entrar</button>
